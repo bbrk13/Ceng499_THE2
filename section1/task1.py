97debbb5cfa7746e426b13c199052c9c59ff389b
Berk ARSLAN <berk_arslan@berks-macbook-pro.local>
2020-04-13 00:39:12 +0300

    task1 is updated.

--- section1/task1.py
+++ section1/task1.py
@@ -1,7 +1,6 @@
-# coding=utf-8
 import csv
-import math
 import random
+from statistics import mode
 
 random.seed(1234)  # fix randomness
 
@@ -15,48 +14,46 @@ def kNN(k, train_set, test_set):
     :param test_set: test set, a list of lists where each nested list is a test instance
     :return: percent accuracy for the test set, e.g., 78.42
     """
-    
-    """
-    rows = []
-    with open(pathOfInput) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            row.append(altitude_value)
-            rows.append(row)
-        # print(rows)
-    csv_file.close()
 
-    """
+    true_positives_and_negatives = 0.0
+    total_number_of_test_instances = 0.0
 
-    train_instances = []
-    with open(train_set) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            train_instances.append(row)
-
-    test_instances = []
-    with open(test_set) as csv_file_2:
-        csv_reader_2 = csv.reader(csv_file_2, delimiter=',')
-        for row in csv_reader_2:
-            test_instances.append(row)
-
-    neighbors_distance_list = []  # sublist - first element = index, second = distance, third = label
-    for line in train_instances:
-        # print(line)
+    for index4, line in enumerate(test_set):
+        total_number_of_test_instances += 1.0
+        neighbors_distance_list = []  # sublist - first element = index, second = distance, third = label
         line_length = len(line)
-        # print("length = {}".format(line_length))
-        label = line[line_length - 1]
-        # print("label of instance = {}".format(label))
-        for feature in line[:line_length - 1]:
-            tmp_distance = 0.0
-            for index, feature_2 in enumerate(line[:line_length - 1]):
-                if feature == feature_2:
-                    continue
-                else:
-                    tmp_distance += euclidean_distance(float(feature), float(feature_2))
-            # calculate one line with all others
-            # when calculating chech each features
+        real_label = line[line_length - 1]
+
+        for index1, line2 in enumerate(train_set):
+            total_distance = 0.0
+            line_length2 = len(line2)
+            label2 = line2[line_length2 - 1]
+            for index2, feature in enumerate(line2[:line_length2 - 1]):
+                tmp_distance = euclidean_distance(float(feature), float(line[index2]))
+                total_distance += tmp_distance
+            tmp_list = []
+            tmp_list.append(index1)
+            tmp_list.append(total_distance)
+            tmp_list.append(label2)
+            neighbors_distance_list.append(tmp_list)
+
+        neighbors_distance_list.sort(key=get_its_distance)
+        label_list = []
+        for index3 in range(k):
+            label_list.append(neighbors_distance_list[index3][2])
+
+        predicted_label = mode(label_list)
+        if predicted_label == real_label:
+            true_positives_and_negatives += 1.0
+
+    test_accuracy = true_positives_and_negatives / total_number_of_test_instances
+    return test_accuracy
+
     pass
+
+
+def get_its_distance(list_parameter):
+    return list_parameter[1]
 
 
 def find_best_k(train_set, test_set, num_folds):
@@ -74,11 +71,75 @@ def find_best_k(train_set, test_set, num_folds):
     :param num_folds: the K value in K-fold cross validation
     :return: a tuple, best k value and percent accuracy for the test set using the best k value, e.g., (3, 80.06)
     """
+
+    total_data = train_set
+    length_of_total_data = len(total_data)
+    batch_size = length_of_total_data / num_folds
+    batch_size = round(batch_size)
+    new_train_data = []
+    new_test_data = []
+
+    for index1 in range(num_folds):
+        tmp_train_list = []
+        tmp_test_list = []
+
+        for index2, entry in enumerate(total_data):
+            if (index1 * batch_size) <= index2 < (index1 + 1) * batch_size:
+                tmp_test_list.append(entry)
+            else:
+                tmp_train_list.append(entry)
+
+        new_train_data.append(tmp_train_list)
+        new_test_data.append(tmp_test_list)
+
+    result_list = []
+
+    for index4 in range(1, 20, 2):
+        total_test_accuracy = 0.0
+
+        for index3 in range(num_folds):
+            partial_test_accuracy = kNN(index4, new_train_data[index3], new_test_data[index3])
+            total_test_accuracy += partial_test_accuracy
+
+        tmp_tuple = (index4, total_test_accuracy / num_folds)
+        print(tmp_tuple)
+        result_list.append(tmp_tuple)
+
+    result_list.sort(key=get_tuple_second, reverse=True)
+    return result_list[0]
     pass
+
+
+def get_tuple_second(tuple_parameter):
+    return tuple_parameter[1]
 
 
 def euclidean_distance(value1, value2):
     return (value1 ** 2 + value2 ** 2) ** 0.5
 
 
-kNN(3, "task1_test.txt", "task1_train.txt")
+def get_test_instances_from_file(path_of_test_set):
+    test_instances_local = []
+
+    with open(path_of_test_set) as csv_file_2:
+        csv_reader_2 = csv.reader(csv_file_2, delimiter=',')
+        for row in csv_reader_2:
+            test_instances_local.append(row)
+    return test_instances_local
+
+
+def get_train_instances_from_file(path_of_train_set):
+    train_instances_local = []
+
+    with open(path_of_train_set) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            train_instances_local.append(row)
+    return train_instances_local
+
+
+train_instances = get_train_instances_from_file("task1_train.txt")
+test_instances = get_test_instances_from_file("task1_test.txt")
+
+result = find_best_k(train_instances, test_instances, 5)
+print("best k is {} | test accuracy: {} ".format(result[0], result[1]))
---
+++ section2/task2.py
@@ -0,0 +1,4 @@
+import csv
+import random
+from statistics import mode
+
